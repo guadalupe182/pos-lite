513fcd52bc2b625ea1f6046a50166b503e30e740
Guadalupe Adrian Rosas Hinojosa <guadalupe.rhinojosa@alumnos.udg.mx>
2025-11-09 11:05:07 -0600

    chore: Init pos-lite from Spring Initializr

---
+++ src/main/java/com/Gdev/pos_lite/PosLiteApplication.java
@@ -0,0 +1,13 @@
+package com.Gdev.pos_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gdev/pos_lite/PosLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gdev.pos_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
